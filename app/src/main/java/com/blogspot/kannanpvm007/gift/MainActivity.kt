ec915018ca474a04526299690252c13df3414877
icliniq_n6 <kannanramakrishnan>
2023-02-21 18:43:11 +0530

    sample nav graph added

--- app/src/main/java/com/blogspot/kannanpvm007/gift/MainActivity.kt
+++ app/src/main/java/com/blogspot/kannanpvm007/gift/MainActivity.kt
@@ -1,43 +1,88 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.blogspot.kannanpvm007.gift
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.blogspot.kannanpvm007.gift.ui.theme.GiftTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.blogspot.kannanpvm007.gift.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GiftTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    SetUpNavGraph()
                 }
             }
         }
     }
+
+    @Composable
+    fun SetUpNavGraph() {
+        navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "SplashScreen") {
+            composable("HomePage") {
+                HomePage()
+            }
+            composable("SplashScreen") {
+                SplashScreen(navController)
+            }
+
+        }
+    }
 }
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomePage() {
+    Scaffold {
+        Text(
+            text = "Home Page",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun DefaultPreview() {
-    GiftTheme {
-        Greeting("Android")
+fun SplashScreen(navController: NavHostController) {
+    Scaffold {
+        Column {
+
+
+            Text(
+                text = "SplashScreen",
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Button(onClick = {
+                navController.navigate("HomePage")
+            }) {
+                Text(text = "Home Screen")
+            }
+        }
     }
 }
